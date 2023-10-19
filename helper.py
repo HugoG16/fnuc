a5637faa0ac772917fc89d309a3677a8f6b546f0
Hugo <hugogomes.g11@gmail.com>
2023-10-19 16:12:09 +0100

    first commit

---
+++ helper.py
@@ -0,0 +1,91 @@
+"""
+
+FIX UNITS
+
+FIX MASS
+
+"""
+
+
+import numpy as np
+from scipy import optimize
+
+E_C = 1
+EPS = 1
+HBAR = 1
+C = 1
+
+K = E_C**2 / (4*np.pi*EPS)
+
+LOG2 = np.log(2)
+
+class Nuclei:
+    def __init__(self, z, n, hl, hl_e, q, q_e):
+        self.z = z
+        self.n = n
+        self.hl = hl
+        self.hl_e = hl_e
+        self.q = q
+        self.q_e = q_e
+    
+    def __lt__(self, other):
+        return self.hl < other.hl
+    
+    def __eq__(self, other):
+        return self.hl == other.hl
+    
+    def __gt__(self, other):
+        return self.hl > other.hl
+
+class Fit:
+    def __init__(self, f:float, hl:list, Q:list, Z:list, A:list, delta:list ) -> None:
+        """
+        f : hit frequency
+        hl : list of half life's
+        Q : list of Q values
+        Z : list of Z values
+        A : list of A values
+        delta : list of values for the mass defect
+        """
+        hl = np.asarray(hl)
+        Q = np.asarray(Q)
+        Z = np.asarray(Z)
+        A = np.asarray(A)
+        delta = np.asarray(delta)
+
+        self.f = f              # const
+        self.hl = hl            # y
+        self.Q = Q              # x1
+        self.z = 2              # const
+        self.Z2 = Z - 2         # x2
+        self.A = A              # x3
+        self.delta = delta      # x4
+    
+    def bc(self, Q, Z2):
+        return Q / (K* self.z * Z2)
+    
+    def m(self, A, delta):
+        return A + delta / C**2
+    
+    def G(self, Q, Z2, delta, A, a):
+        x = a / self.bc(A, delta)
+
+        c1 = np.sqrt(2 * self.m(A, delta) / HBAR**2 / Q)
+        c2 = self.z * Z2 * K
+        c3 = np.arccos(np.sqrt(x)) - np.sqrt(x*(1-x))
+
+        return c1*c2*c3
+    
+    def P(self, Q, Z2, delta, A, a):
+        return np.exp(-2*self.G(Q, Z2, delta, A, a))
+    
+    def fit_func(self, x, a):
+        Q, Z2, delta, A = x
+        return LOG2 / (self.f * self.P(Q, Z2, delta, A, a))
+    
+
+    def fit(self):
+        x = (self.Q, self.Z2, self.delta, self.A)
+        popt, pcov = optimize.curve_fit(self.fit_func, x, self.hl)
+        print(popt)
+        print(pcov)
---
+++ main.py
@@ -0,0 +1,89 @@
+"""
+name,z,n,halflife(Seconds),d-halflife,alpha(keV),d-alpha
+"""
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from pathlib import Path
+fpath = Path(mpl.get_data_path(), "fonts/ttf/cmr10.ttf")
+import matplotlib.font_manager as font_manager
+font_path = fpath
+font_manager.fontManager.addfont(font_path)
+prop = font_manager.FontProperties(fname=font_path)
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.sans-serif'] = prop.get_name()
+plt.rcParams['font.size'] = 12
+plt.rcParams["axes.formatter.use_mathtext"] = True
+
+from helper import *
+
+data = list()
+equal_z = dict()
+
+## read data
+with open('data.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        z, n, hl, hl_e, q, q_e = row[1], row[2], row[3], row[4], row[5], row[6]
+
+        try:
+            z, n, hl, hl_e, q, q_e = float(z), float(n), float(hl), float(hl_e), float(q), float(q_e)
+        except:
+            z, n, hl, hl_e, q, q_e = float(z), float(n), float(hl), 0, float(q), float(q_e)
+
+        nuclei = Nuclei(z, n, hl, hl_e, q, q_e)
+        data.append(nuclei)
+
+## separete data into equal z lists
+for nuclei in data:
+    try:
+        equal_z[nuclei.z].append(nuclei)
+    except KeyError:
+        equal_z[nuclei.z] = [nuclei]
+
+## sort equal z data based on z
+equal_z = dict(sorted(equal_z.items()))
+
+## sort equal z data based on hl
+for z in equal_z:
+    equal_z[z].sort()
+
+## remove from equal z lists with only one element
+equal_z_temp = equal_z.copy()
+for z in equal_z_temp:
+    if len(equal_z_temp[z]) == 1:
+        equal_z.pop(z)
+
+############ geiger law ############
+if 0:
+    cmap = plt.get_cmap('turbo', 16)
+    for i, z in enumerate(equal_z):
+        q_list = list()
+        hl_list = list()
+
+        for nuclei in equal_z[z]:
+            q_list.append(nuclei.q)
+            hl_list.append(nuclei.hl)
+        
+        q_list = np.asarray(q_list)**(-0.5)
+        hl_list = np.asarray(hl_list)
+
+        plt.semilogy(q_list, hl_list, label=f"Z={int(z)}",c=cmap(i), marker='.')
+
+
+    plt.ylabel('Half-life (Seconds)')
+    plt.xlabel('Q (keVs) ^ -1/2')
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.grid()
+    plt.tight_layout()
+    # plt.savefig('geiger.png', dpi=300)
+    plt.show()
+
+
+
+############ fit func ############
+
+fitter = Fit(1, [1,1], [1,1], [1,1], [1,1], [0.1,0.1])
+fitter.fit()
